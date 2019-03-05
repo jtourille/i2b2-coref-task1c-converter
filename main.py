8a5232209d7be6b95b2950838b3e2536f20b9670
Julien Tourille <julien@backward.io>
2019-03-05 10:39:36 +0100

    Add RUN-TO-CONLL launcher

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import logging
 import os
 import re
@@ -7,8 +8,8 @@ import sys
 import time
 from datetime import timedelta
 
-from i2b2.brat import generate_brat_files
-from i2b2.conll import conll_to_i2b2
+from i2b2.brat import generate_brat_files, i2b2_to_brat, generate_brat_conf_files
+from i2b2.conll import conll_to_i2b2, conll_files_task1c
 from i2b2.conll import create_conll_files
 from i2b2.offset import create_offset_mapping
 from i2b2.prepare import prepare_data_task1c
@@ -75,6 +76,18 @@ if __name__ == "__main__":
                                type=str, required=True)
     parser_remove.add_argument("--overwrite", help="Overwrite existing documents", dest="overwrite",
                                action="store_true")
+
+    parser_run_to_conll = subparsers.add_parser('RUN-TO-CONLL', help="Convert one run to CoNLL")
+    parser_run_to_conll.add_argument("--input-dir", help="Path where the run is stored", dest="input_dir",
+                                     type=str, required=True)
+    parser_run_to_conll.add_argument("--output-dir", help="Path where the CoNLL version will be stored",
+                                     dest="output_dir", type=str, required=True)
+    parser_run_to_conll.add_argument("--gs-conll-dir", help="Path where the gs CoNLL files are stored",
+                                     dest="gs_conll_dir", type=str, required=True)
+    parser_run_to_conll.add_argument("--mapping-file", help="Character mapping filepath", dest="mapping_file",
+                                     type=str, required=True)
+    parser_run_to_conll.add_argument("--overwrite", help="Overwrite existing documents", dest="overwrite",
+                                     action="store_true")
 
     args = parser.parse_args()
 
@@ -287,6 +300,66 @@ if __name__ == "__main__":
         ensure_dir(output_dir)
 
         replace_semantic_types(input_dir, output_dir)
+
+    elif args.subparser_name == "RUN-TO-CONLL":
+
+        if not os.path.isfile(os.path.abspath(args.mapping_file)):
+            raise NotADirectoryError("The mapping file does not exist: {}".format(
+                os.path.abspath(args.mapping_file)
+            ))
+
+        if not os.path.isdir(os.path.abspath(args.input_dir)):
+            raise IsADirectoryError("The input directory does not exist: {}".format(
+                os.path.abspath(args.input_dir)
+            ))
+
+        if not os.path.isdir(os.path.abspath(args.gs_conll_dir)):
+            raise IsADirectoryError("The GS CoNLL directory does not exist: {}".format(
+                os.path.abspath(args.gs_conll_dir)
+            ))
+
+        if not args.overwrite:
+            if os.path.isdir(os.path.abspath(args.output_dir)):
+                logging.info("The output directory already exists, use the appropriate launcher flag to overwrite")
+                raise IsADirectoryError("The output directory already exists: {}".format(
+                    os.path.abspath(args.output_dir)
+                ))
+
+        if os.path.isdir(os.path.abspath(os.path.abspath(args.output_dir))):
+            shutil.rmtree(os.path.abspath(os.path.abspath(args.output_dir)))
+
+        ensure_dir(os.path.abspath(args.output_dir))
+
+        brat_dir = os.path.join(os.path.abspath(args.output_dir), "brat")
+        conll_dir = os.path.join(os.path.abspath(args.output_dir), "conll")
+
+        ensure_dir(brat_dir)
+        ensure_dir(conll_dir)
+
+        # Loading character mapping
+        with open(os.path.abspath(args.mapping_file), "r", encoding="UTF-8") as input_file:
+            char_mapping = json.load(input_file)
+
+        i2b2_to_brat(os.path.abspath(args.input_dir),
+                     brat_dir,
+                     char_mapping)
+
+        generate_brat_conf_files(brat_dir)
+
+        conll_files_task1c(brat_dir=brat_dir,
+                           gs_dir=os.path.abspath(args.gs_conll_dir),
+                           output_dir=conll_dir)
+
+        target_conll_file = os.path.join(os.path.abspath(args.output_dir), "all.conll")
+
+        with open(target_conll_file, "w", encoding="UTF-8") as output_file:
+            for dirname in os.listdir(conll_dir):
+                for root, dirs, files in os.walk(os.path.join(conll_dir, dirname)):
+                    for filename in files:
+                        if re.match("^.*\.conll$", filename):
+                            with open(os.path.join(root, filename), "r", encoding="UTF-8") as input_file:
+                                for line in input_file:
+                                    output_file.write(line)
 
     end = time.time()
 
